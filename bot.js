b87635886b9773ad6ba341b3bfc5deab64f1f9ae
Florian <florian.gge@gmail.com>
2017-07-25 13:01:37 +0200

    init bot

---
+++ bot.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * Created by Florian Guigue on 25/07/2017.
+ */
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+client.on('ready', function () {
+    console.log('Trivia bot started on ');
+});
+
+client.login("MzM5MzUzMDYwMTg0ODE3NjY1.DFiyPQ.-26n8W3h_2Q-1nnTFCrLp0tT_SQ");
