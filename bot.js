4051812615e4912c8483657053fae9616af0b9d1
Florian <florian.gge@gmail.com>
2017-07-25 13:37:34 +0200

    server welcome

--- bot.js
+++ bot.js
@@ -8,7 +8,13 @@ const client = new Discord.Client();
 
 
 client.on('ready', function () {
-    console.log('Trivia bot started on ');
-});
-
-client.login("MzM5MzUzMDYwMTg0ODE3NjY1.DFiyPQ.-26n8W3h_2Q-1nnTFCrLp0tT_SQ");
+        console.log('Trivia bot is running since : ' + moment().format('DD-MM-YYYY HH:mm:ss'))
+        client.user.setGame('Trivia game').catch(console.error);
+    })
+    .on('guildMemberAdd', function (member) {
+        member.guild.defaultChannel.send(${member} + " vient d'arriver sur le serveur");
+    })
+    .on('message', function (member) {
+    
+    })
+    .login("MzM5MzUzMDYwMTg0ODE3NjY1.DFiyPQ.-26n8W3h_2Q-1nnTFCrLp0tT_SQ");
